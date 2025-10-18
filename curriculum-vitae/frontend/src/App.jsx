7702e20f6d5fbfd376507c0bfe76865025829bba
Mario <gpmario@gmail.com>
2025-10-18 21:09:39 +0200

    Cambiar BrowserRouter por HashRouter para GitHub Pages

--- curriculum-vitae/frontend/src/App.jsx
+++ curriculum-vitae/frontend/src/App.jsx
@@ -1,13 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import CV from "./pages/CV";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<CV />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
