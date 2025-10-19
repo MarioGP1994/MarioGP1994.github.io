c4a23fad641fb7a2259bd1388649cd0091e32407
Mario <gpmario@gmail.com>
2025-10-19 12:03:06 +0200

    Organiza assets en src, corrige rutas y actualiza CV

--- curriculum-vitae/frontend/src/main.jsx
+++ curriculum-vitae/frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './input.css'; 
-import './globals.css'; 
+import './globals.css';
+import './input.css';  
 import './index.css';  
 import App from './App.jsx';
 
